294bc5e997c9516a39b15ed0d857d1186185d57e
Caner AL <caneral5858@gmail.com>
2019-11-23 18:31:06 +0300

    First commit.

---
+++ RastgeleFilm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RastgeleFilm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
