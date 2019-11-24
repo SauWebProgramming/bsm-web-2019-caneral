33c5665a2a9ce960277b6056b30c5e5b88265170
Caner AL <caneral5858@gmail.com>
2019-11-24 21:00:47 +0300

    Sanal veritabanı ve model oluşturuldu. Rastgele Film mekanizması oluşturuldu.

--- RastgeleFilm/Controllers/HomeController.cs
+++ RastgeleFilm/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace RastgeleFilm.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(MovieRepository.Movies);
         }
 
         
